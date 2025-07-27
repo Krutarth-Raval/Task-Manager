8c9efcb23e63a081dbe7f81aec0f42a0930cbca0
Krutarth-Raval <ravalkrutarth95@gmail.com>
2025-07-27 18:13:40 +0530

    Report Controller and Task Controller created

---
+++ Backend/Controllers/reportController.js
@@ -0,0 +1,127 @@
+const Task = require("../models/Task");
+const User = require("../models/User");
+const excelJs = require("exceljs");
+
+//@desc  Export all task  as Excel file
+//@route  POST /api/reports/export/tasks
+//@access Private (Admin)
+
+const exportTasksReport = async (req, res) => {
+  try {
+    const tasks = await Task.find().populate("assignedTo", "name email");
+    const users = new excelJs.Workbook();
+    const worksheet = users.addWorksheet("Tasks Report");
+
+    worksheet.columns = [
+      { header: "Task ID", key: "_id", width: 25 },
+      { header: "Title", key: "title", width: 30 },
+      { header: "Description", key: "description", width: 50 },
+      { header: "Priority", key: "priority", width: 15 },
+      { header: "Status", key: "status", width: 20 },
+      { header: "Due Date", key: "dueDate", width: 20 },
+      { header: "Assigned To", key: "assignedTo.name", width: 30 },
+    ];
+
+    tasks.forEach((task) => {
+      const assignedTo = task.assignedTo
+        .map((user) => `${user.name} (${user.email})`)
+        .join(", ");
+      worksheet.addRow({
+        _id: task._id,
+        title: task.title,
+        description: task.description,
+        priority: task.priority,
+        status: task.status,
+        dueDate: task.dueDate.toISOString().split("T")[0],
+        assignedTo: assignedTo || "Unassigned",
+      });
+    });
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=TasksReport.xlsx"
+    );
+    return worksheet.xlsx.write(res).then(() => {
+      res.end();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error exporting tasks reports", error: error.message });
+  }
+};
+
+//@desc  Export all user-task report  as Excel file
+//@route  POST /api/reports/export/user
+//@access Private (Admin)
+
+const exportUsersReport = async (req, res) => {
+  try {
+    const users = await User.find().select("name email _id").lean();
+    const userTasks = await Task.find().populate(
+      "assignedTo",
+      "name email _id"
+    );
+    const userTaskMap = {};
+    users.forEach((user) => {
+      userTaskMap[user._id] = {
+        name: user.name,
+        email: user.email,
+        tasksCount: 0,
+        pendingTasks: 0,
+        inProgressTasks: 0,
+        completedTasks: 0,
+      };
+    });
+    userTasks.forEach((task) => {
+      if (task.assignedTo) {
+        task.assignedTo.forEach((assignedUser) => {
+          if (userTaskMap[assignedUser._id]) {
+            userTaskMap[assignedUser._id].tasksCount += 1;
+          } else if (task.status === "In Progress") {
+            userTaskMap[assignedUser._id].inProgressTasks += 1;
+          } else if (task.status === "Completed") {
+            userTaskMap[assignedUser._id].completedTasks += 1;
+          }
+        });
+      }
+    });
+
+    const workbook = new excelJs.Workbook();
+    const worksheet = workbook.addWorksheet("User Task Report");
+
+    worksheet.columns = [
+      { header: "User Name", key: "name", width: 30 },
+      { header: "Email", key: "email", width: 40 },
+      { header: "Total Assigned Tasks", key: "taskCount", width: 20 },
+      { header: "Pending Tasks", key: "pendingTasks", width: 20 },
+      { header: "In Progress Tasks", key: "InProgressTasks", width: 20 },
+      { header: "Completed Tasks", key: "CompletedTasks", width: 20 },
+    ];
+
+    Object.values(userTaskMap).forEach((user) => {
+      worksheet.addRow(user);
+    });
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="UserTaskReport.xlsx"`
+    );
+    return worksheet.xlsx.write(res).then(() => {
+      res.end();
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error exporting tasks reports", error: error.message });
+  }
+};
+
+module.exports = { exportTasksReport, exportUsersReport };
--- Backend/Controllers/taskController.js
+++ Backend/Controllers/taskController.js
@@ -5,6 +5,71 @@ const Task = require("../models/Task");
 //@access Private
 const getTasks = async (req, res) => {
   try {
+    const { status } = req.query;
+    let filter = {};
+
+    if (status) {
+      filter.status = status;
+    }
+
+    let tasks;
+
+    if (req.user.role === "admin") {
+      tasks = await Task.find(filter).populate(
+        "assignedTo",
+        "name email profileImageUrl"
+      );
+    } else {
+      tasks = await Task.find({ ...filter, assignedTo: req.user._id }).populate(
+        "assignedTo",
+        "name email profileImageUrl"
+      );
+    }
+
+    //add completed todoCheckList count to each task
+
+    tasks = await Promise.all(
+      tasks.map(async (task) => {
+        const completedCount = task.todoCheckList.filter(
+          (item) => item.completed
+        ).length;
+        return { ...task._doc, completedTodoCount: completedCount };
+      })
+    );
+
+    // Status summary counts
+
+    const allTasks = await Task.countDocuments(
+      req.user.role === "admin" ? {} : { assignedTo: req.user._id }
+    );
+
+    const notStartedTasks = await Task.countDocuments({
+      ...filter,
+      status: "Not Started",
+      ...(req.user.role !== "admin" && { assignedTo: req.user._id }),
+    });
+
+    const inProgressTasks = await Task.countDocuments({
+      ...filter,
+      status: "In Progress",
+      ...(req.user.role !== "admin" && { assignedTo: req.user._id }),
+    });
+
+    const completedTasks = await Task.countDocuments({
+      ...filter,
+      status: "Completed",
+      ...(req.user.role !== "admin" && { assignedTo: req.user._id }),
+    });
+
+    return res.status(200).json({
+      tasks,
+      statusSummary: {
+        all: allTasks,
+        notStartedTasks,
+        inProgressTasks,
+        completedTasks,
+      },
+    });
   } catch (error) {
     return res
       .status(500)
@@ -17,6 +82,15 @@ const getTasks = async (req, res) => {
 //@access Private
 const getTasksById = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profileImageUrl"
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    return res.status(201).json(task);
   } catch (error) {
     return res
       .status(500)
@@ -29,6 +103,33 @@ const getTasksById = async (req, res) => {
 //@access Private(Admin)
 const createTask = async (req, res) => {
   try {
+    const {
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      attachments,
+      todoCheckList,
+    } = req.body;
+
+    if (!Array.isArray(assignedTo)) {
+      return res
+        .status(400)
+        .json({ message: "assignedTo must be an array of user IDs" });
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      priority,
+      dueDate,
+      assignedTo,
+      createdBy: req.user._id,
+      attachments,
+      todoCheckList,
+    });
+    return res.status(201).json({ message: "Task Created Successfully", task });
   } catch (error) {
     return res
       .status(500)
@@ -41,6 +142,29 @@ const createTask = async (req, res) => {
 //@access Private
 const updateTask = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found." });
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.priority = req.body.priority || task.priority;
+    task.dueDate = req.body.dueDate || task.dueDate;
+    task.attachments = req.body.attachments || task.attachments;
+    task.todoCheckList = req.body.todoCheckList || task.todoCheckList;
+    if (req.body.assignedTo) {
+      if (!Array.isArray(req.body.assignedTo)) {
+        return res
+          .status(400)
+          .json({ message: "assignedTo Must be An Array Of User IDs" });
+      }
+      task.assignedTo = req.body.assignedTo;
+    }
+
+    const updatedTask = await task.save();
+    return res
+      .status(200)
+      .json({ message: "Task Updated Successfully", updatedTask });
   } catch (error) {
     return res
       .status(500)
@@ -53,6 +177,13 @@ const updateTask = async (req, res) => {
 //@access Private(admin)
 const deleteTask = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    await task.deleteOne();
+
+    res.json({ message: "Task deleted successfully" });
   } catch (error) {
     return res
       .status(500)
@@ -65,6 +196,23 @@ const deleteTask = async (req, res) => {
 //@access Private
 const updateTaskStatus = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    const isAssigned = task.assignedTo.some(
+      (userId) => userId.toString() === req.user._id.toString()
+    );
+    if (!isAssigned && req.user.role !== "admin") {
+      return res.status(403).json({ message: "Not Authorized" });
+    }
+    task.status = req.body.status || task.status;
+    if (task.status === "Completed") {
+      task.todoCheckList.forEach((item) => {
+        item.completed = true;
+      });
+      task.progress = 100;
+    }
+    await task.save();
+    return res.status(201).json({ message: "Task Status Updated", task });
   } catch (error) {
     return res
       .status(500)
@@ -77,6 +225,48 @@ const updateTaskStatus = async (req, res) => {
 //@access Private
 const updateTaskChecklist = async (req, res) => {
   try {
+    const { todoCheckList } = req.body;
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task Not Found" });
+
+    if (!task.assignedTo.includes(req.user._id) && req.user.role !== "admin") {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update checklist" });
+    }
+
+    task.todoCheckList = todoCheckList; //replace with updated checklist
+
+    //auto-update progress based onchecklist complitinon
+
+    const completedCount = task.todoCheckList.filter(
+      (item) => item.completed
+    ).length;
+    const totalItems = task.todoCheckList.length;
+    task.progress =
+      totalItems > 0 ? Math.round((completedCount / totalItems) * 100) : 0;
+
+    //auto-mark task as comeplted if all items are cecked
+
+    if (task.progress === 100) {
+      task.status = "Completed";
+    } else if (task.progress > 0) {
+      task.status = "In Progress";
+    } else {
+      task.status = "Not Started";
+    }
+
+    await task.save();
+
+    const updatedTask = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profileImageUrl"
+    );
+
+    return res
+      .status(200)
+      .json({ message: "Task Checklist Updated", updatedTask });
   } catch (error) {
     return res
       .status(500)
@@ -89,6 +279,69 @@ const updateTaskChecklist = async (req, res) => {
 //@access Private
 const getDashboardData = async (req, res) => {
   try {
+    //fetch statistics
+
+    const totalTasks = await Task.countDocuments();
+    const pendingTasks = await Task.countDocuments({ status: "Not Started" });
+    const competedTasks = await Task.countDocuments({ status: "Completed" });
+    const overDueTasks = await Task.countDocuments({
+      status: { $ne: "Completed" },
+      dueDate: { $lt: new Date() },
+    });
+    //ensure all possible statuses are includes
+    const taskStatuses = ["Not Started", "In Progress", "Completed"];
+    const taskDistributionRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    const taskDistribution = taskStatuses.reduce((acc, status) => {
+      const formattedKey = status.replace(/\s+/g, ""); //Remove spaces from response keys
+      acc[formattedKey] =
+        taskDistributionRaw.find((item) => item._id === status)?.count || 0;
+      return acc;
+    }, {});
+    taskDistribution["All"] = totalTasks; //add total count to taskDistribution
+
+    //ensure all possible levels are included
+
+    const taskPriorities = ["Low", "High", "Medium"];
+    const taskPriorityLevelsRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$priority",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
+      acc[priority] =
+        taskPriorityLevelsRaw.find((item) => item._id === priority)?.count || 0;
+      return acc;
+    }, {});
+    //fetch recent 10 tasks
+    const recentTasks = await Task.find()
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .select("title status priority dueDate createdAt");
+
+    return res.status(200).json({
+      statistics: {
+        totalTasks,
+        pendingTasks,
+        competedTasks,
+        overDueTasks,
+      },
+      charts: {
+        taskDistribution,
+        taskPriorityLevels,
+      },
+      recentTasks,
+    });
   } catch (error) {
     return res
       .status(500)
@@ -101,6 +354,70 @@ const getDashboardData = async (req, res) => {
 //@access Private
 const getUserDashboardData = async (req, res) => {
   try {
+    const userId = req.user._id; // Only fetch data of logged-in user
+
+    //fetch statistics for user-specific tasks
+
+    const totalTasks = await Task.countDocuments({ assignedTo: userId });
+    const pendingTasks = await Task.countDocuments({
+      assignedTo: userId,
+      status: "Not Started",
+    });
+    const competedTasks = await Task.countDocuments({
+      assignedTo: userId,
+      status: "completed",
+    });
+    const overDueTasks = await Task.countDocuments({
+      assignedTo: userId,
+      status: { $ne: "Completed" },
+      dueDate: { $lt: new Date() },
+    });
+
+    //task distribution bt status
+
+    const taskStatuses = ["Not Started", "In Progress", "Completed"];
+    const taskDistributionRaw = await Task.aggregate([
+      { $match: { assignedTo: userId } },
+      { $group: { _id: "$status", count: { $sum: 1 } } },
+    ]);
+
+    const taskDistribution = taskStatuses.reduce((acc, status) => {
+      const formattedKey = status.replace(/\s+/g, "");
+      acc[formattedKey] =
+        taskDistributionRaw.find((item) => item._id === status)?.count || 0;
+      return acc;
+    }, {});
+    taskDistribution["All"] = totalTasks;
+
+    //task distribution by priority
+    const taskPriorities = ["Low", "Medium", "High"];
+    const taskPriorityLevelsRaw = await Task.aggregate([
+      { $match: { assignedTo: userId } },
+      { $group: { _id: "$priority", count: { $sum: 1 } } },
+    ]);
+    const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
+      taskPriorityLevelsRaw.find((item) => item._id === priority)?.count || 0;
+      return acc;
+    }, {});
+    //fetch recent 10 tasks for the logged-in user
+    const recentTasks = await Task.find({ assignedTo: userId })
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .select("title status priority dueDate createdAt");
+
+    return res.status(200).json({
+      statistics: {
+        totalTasks,
+        pendingTasks,
+        competedTasks,
+        overDueTasks,
+      },
+      charts: {
+        taskDistribution,
+        taskPriorityLevels,
+      },
+      recentTasks,
+    });
   } catch (error) {
     return res
       .status(500)
